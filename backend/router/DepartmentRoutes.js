987609912ec01111d3b56feb68ab9ea9c6a98b67
himanshu0752 <your-email@example.com>
2025-06-09 23:01:06 +0530

    Add Netlify config

--- backend/router/DepartmentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createDepartment, listDepartments } = require('../controllers/departmentController');
-
-router.post('/', createDepartment);     // Create department
-router.get('/', listDepartments);       // Get list of departments
-
-module.exports = router;
