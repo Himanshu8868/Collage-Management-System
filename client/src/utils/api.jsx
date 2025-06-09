c6fe5feddeb08d871c7449933855b6312e24500d
himanshu0752 <your-email@example.com>
2025-06-09 15:38:46 +0530

    chanages

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -1,6 +1,6 @@
-const API_BASE_URL = "https://collage-management-system-2.onrender.com";  // Backend URL
+const VITE_API_BASE_URL = "https://collage-management-system-2.onrender.com";  // Backend URL
 
 export const fetchData = async () => {
-  const response = await fetch(`${API_BASE_URL}/api/data`);
+  const response = await fetch(`${VITE_API_BASE_URL}/api/data`);
   return response.json();
 };
