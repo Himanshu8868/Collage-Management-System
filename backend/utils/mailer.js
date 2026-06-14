30b3aeb8dc2f086362239d556f460b24d0b2921b
Priyanshu <priyanshurawat22@maya.edu.in>
2026-06-14 20:52:34 +0530

    prob

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -22,39 +22,39 @@
 // };
 
 // module.exports = sendEmail;
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-const sendEmail = async (to, subject, html) => {
-    try {
-        // Create transporter
-        const transporter = nodemailer.createTransport({
-            host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-            port: process.env.EMAIL_PORT || 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-            tls: {
-                rejectUnauthorized: false // For development only
-            }
-        });
+// const sendEmail = async (to, subject, html) => {
+//     try {
+//         // Create transporter
+//         const transporter = nodemailer.createTransport({
+//             host: process.env.EMAIL_HOST || 'smtp.gmail.com',
+//             port: process.env.EMAIL_PORT || 587,
+//             secure: false, // true for 465, false for other ports
+//             auth: {
+//                 user: process.env.EMAIL_USER,
+//                 pass: process.env.EMAIL_PASS,
+//             },
+//             tls: {
+//                 rejectUnauthorized: false // For development only
+//             }
+//         });
 
-        // Send mail
-        const info = await transporter.sendMail({
-            from: `"CampusPro" <${process.env.EMAIL_USER}>`,
-            to: to,
-            subject: subject,
-            html: html
-        });
+//         // Send mail
+//         const info = await transporter.sendMail({
+//             from: `"CampusPro" <${process.env.EMAIL_USER}>`,
+//             to: to,
+//             subject: subject,
+//             html: html
+//         });
 
-        console.log('Email sent:', info.messageId);
-        return true;
-    } catch (error) {
-        console.error('Email error:', error);
-        // Don't throw error in production - just log it
-        return false;
-    }
-};
+//         console.log('Email sent:', info.messageId);
+//         return true;
+//     } catch (error) {
+//         console.error('Email error:', error);
+//         // Don't throw error in production - just log it
+//         return false;
+//     }
+// };
 
-module.exports = sendEmail;
+// module.exports = sendEmail;
